5522f545641c7827ca025a8a987e6c41fe98cfb7
Jocelyn Moore <hello@jociemoore.com>
2017-05-01 22:52:56 -0400

    180 Todo app update

---
+++ database_persistence.rb
@@ -0,0 +1,110 @@
+require "pg"
+require "pry"
+
+class DatabasePersistence
+  def initialize(logger)
+    @db = PG.connect(dbname: "todos")
+    @logger = logger
+  end
+
+  def query(statment, *params)
+    @logger.info "#{statment}: #{params}"
+    @db.exec_params(statment, params)
+  end
+
+  def find_list(id)
+    sql = <<-SQL
+      SELECT lists.*, 
+      COUNT(NULLIF(todos.completed, true)) AS todos_remaining_count, 
+      COUNT(todos.id) AS todos_count 
+      FROM lists 
+      LEFT JOIN todos ON todos.list_id = lists.id 
+      WHERE lists.id = $1
+      GROUP BY lists.id
+      ORDER BY lists.name;
+    SQL
+    result = query(sql, id)
+
+    tuple = result.first
+    tuple_to_list_hash(tuple)
+  end
+
+  def all_lists
+    sql = <<-SQL
+      SELECT lists.*, 
+      COUNT(NULLIF(todos.completed, true)) AS todos_remaining_count, 
+      COUNT(todos.id) AS todos_count 
+      FROM lists 
+      LEFT JOIN todos ON todos.list_id = lists.id 
+      GROUP BY lists.id
+      ORDER BY lists.name;
+    SQL
+    result = query(sql)
+
+    result.map do |tuple|
+      tuple_to_list_hash(tuple)
+    end
+  end
+
+  def create_new_list(list_name)
+    sql = "INSERT INTO lists (name) VALUES ($1)"
+    query(sql, list_name)
+  end
+
+  def delete_list(id)
+    list_sql = "DELETE FROM lists WHERE id = $1"
+    todo_sql = "DELETE FROM todos WHERE list_id = $1"
+    query(todo_sql, id)
+    query(list_sql, id)
+  end
+
+  def update_list_name(id, new_name)
+    sql = "UPDATE lists SET name = $1 WHERE id = $2"
+    query(sql, new_name, id )
+  end
+
+  def create_new_todo(list_id, todo_name)
+    sql = "INSERT INTO todos (name, list_id) VALUES ($1, $2)"
+    query(sql, todo_name, list_id)
+  end
+
+  def delete_todo_from_list(list_id, todo_id)
+    sql = "DELETE FROM todos WHERE id = $1 AND list_id = $2"
+    query(sql, todo_id, list_id)
+  end
+
+  def update_todo_status(list_id, todo_id, new_status)
+    sql = "UPDATE todos SET completed = $1 WHERE id = $2 AND list_id = $3"
+    query(sql, new_status, todo_id, list_id)
+  end
+
+  def mark_all_todos_as_completed(list_id)
+    sql = "UPDATE todos SET completed = 't' WHERE list_id = $1"
+    query(sql, list_id)
+  end
+
+  def find_todos_for_list(list_id)
+    todo_sql = "SELECT * FROM todos WHERE list_id = $1"
+    todo_result = query(todo_sql, list_id)
+
+    todo_result.each_with_object([]) do |todo, array|
+      array << {
+        id: todo["id"].to_i, 
+        name: todo["name"], 
+        list_id: todo["list_id"], 
+        completed: todo["completed"] == "t"}
+    end
+  end
+
+  private
+
+  def tuple_to_list_hash(tuple)
+    { 
+      id: tuple["id"].to_i,
+      name: tuple["name"], 
+      todos_count: tuple["todos_count"].to_i,
+      todos_remaining_count: tuple[
+        "todos_remaining_count"].to_i
+    }
+  end
+end
